83691643b6df0e1a4c6fc8d460517ec155cc194d
Benedikt Wüller <me@benedikt.dev>
2023-01-18 19:02:13 +0100

    Do not stop webserver if no ping is received

--- src/main/kotlin/dev/benedikt/plutos/Plutos.kt
+++ src/main/kotlin/dev/benedikt/plutos/Plutos.kt
@@ -1,24 +1,8 @@
 package dev.benedikt.plutos
 
 import dev.benedikt.plutos.api.WebServer
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 fun main() {
     DatabaseService.setup()
-
-    WebServer.start()
-
-    // Stop server if no client is connected.
-    GlobalScope.launch {
-        delay(10000L)
-        while (true) {
-            if (System.currentTimeMillis() - WebServer.lastPing > 10000L) break
-            delay(1000L)
-        }
-        WebServer.stop()
-    }
-
-    WebServer.wait()
+    WebServer.run()
 }
--- src/main/kotlin/dev/benedikt/plutos/api/WebServer.kt
+++ src/main/kotlin/dev/benedikt/plutos/api/WebServer.kt
@@ -2,16 +2,12 @@ package dev.benedikt.plutos.api
 
 import dev.benedikt.plutos.api.routing.*
 import dev.benedikt.plutos.api.structure.*
-import dev.benedikt.plutos.importers.Importer
-import dev.benedikt.plutos.importers.statements.CommerzbankImporter
-import dev.benedikt.plutos.importers.statements.SparkasseImporter
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.application.hooks.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
-import io.ktor.server.http.content.*
 import io.ktor.server.plugins.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
@@ -19,23 +15,16 @@ import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import io.ktor.server.routing.route
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.polymorphic
-import kotlinx.serialization.modules.subclass
-import org.jetbrains.exposed.exceptions.ExposedSQLException
-import java.io.File
 
 object WebServer {
 
     var lastPing: Long = System.currentTimeMillis()
     private lateinit var server: ApplicationEngine
 
-    fun start(port: Int = 8143) {
+    fun run(port: Int = 8143) {
         server = embeddedServer(CIO, port = port, module = Application::module)
-        server.start(false)
+        server.start(true)
     }
-
-    fun wait() = server.start(true)
 
     fun stop() = server.stop()
 
